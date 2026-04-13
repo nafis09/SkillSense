ed27c603e9cad2da102b96772bfef50abcaaffd8
nafis09 <nafisahmed003@gmail.com>
2026-04-13 12:34:01 +0600

    feat: add home route with landing page content

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,19 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "SkillSense" },
+    { name: "description", content: "Welcome to SkillSense!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review Your Submissions & Check AI-Powered Feedback.</h2>
+      </div>
+    </section>
+  </main>
 }
