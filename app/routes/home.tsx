1e30c0d0c43f5de08ed35a48225c2af7a455e5a0
nafis09 <nafisahmed003@gmail.com>
2026-04-13 14:54:04 +0600

    feat: implement home route with resume dashboard, navbar, and card components including supporting types and constants

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,7 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,11 +12,20 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <section className="main-section">
+    <Navbar />
+    <section className="main-section py-16">
       <div className="page-heading">
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review Your Submissions & Check AI-Powered Feedback.</h2>
       </div>
+
+          {resumes.length > 0 && (
+      <div className="resumes-section">
+        {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume} />
+        ))}
+      </div>
+    )}
     </section>
   </main>
 }
