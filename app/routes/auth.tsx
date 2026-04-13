87bbe108b50fc4e68a7a6fca8d867a9dc62b5beb
nafis09 <nafisahmed003@gmail.com>
2026-04-13 15:38:27 +0600

    feat: integrate Puter.js SDK with Zustand store for authentication, filesystem, and AI services

---
+++ app/routes/auth.tsx
@@ -0,0 +1,55 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+
+export const meta = () => {
+  return [
+    { title: "SkillSense | Auth" },
+    { name: "description", content: "Log Into Your Account" },
+  ];
+};
+
+const auth = () => {
+    const { isLoading, auth } = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(auth.isAuthenticated) navigate(next);
+    }, [auth.isAuthenticated, next]);
+    
+  return (
+    <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+      <div className="gradient-border shadow-lg">
+        <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+            <div className="flex flex-col items-center gap-2 text-center">
+                <h1>Welcome to SkillSense</h1>
+                <p>Log in to your account to get started</p>
+            </div>
+            <div>
+                {isLoading ? (
+                    <button className="auth-button animate-pulse">
+                        <p>Signing You In....</p>
+                    </button>
+                ) : (
+                    <>
+                        {auth.isAuthenticated ? (
+                            <button className="auth-button" onClick={auth.signOut}>
+                                <p>Log Out</p>
+                            </button>
+                        ) : (
+                            <button className="auth-button" onClick={auth.signIn}>
+                                <p>Log In</p>
+                            </button>
+                        )}
+                    </>
+                )}
+            </div>
+        </section>
+      </div>
+    </main>
+  )
+}
+
+export default auth
